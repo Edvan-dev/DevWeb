b98a9a521a84f1a99454932c4f832fa6becebfaf
Edvan-dev <edvsl@hotmail.com>
2025-03-16 09:27:26 -0300

    Delete old-project

--- Projeto_2024.2/Up_detector/public/js/register.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-        if (params.has('error') && params.get('error') === 'usuario ja existe') {
-          document.getElementById('errorMessage').style.display = 'block';
-        }
